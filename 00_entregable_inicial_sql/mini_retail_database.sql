13451674e7f588d994eea988c5f21d88f1eb6648
nospinapolania-maker <n.ospinapolania@gmail.com>
2026-05-19 21:30:08 -0500

    Insert sample data into retail tables

--- 00_entregable_inicial_sql/mini_retail_database.sql
+++ 00_entregable_inicial_sql/mini_retail_database.sql
@@ -36,3 +36,50 @@ CREATE TABLE ventas (
     FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
     FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
 );
+
+-- 6. Ingresar datos a la tabla clientes
+INSERT INTO clientes (nombre, apellido, edad, ciudad, correo) VALUES
+('Laura', 'Barragán', 27, 'Bogotá', 'lb1999@hotmail.com'),
+('Carlos', 'Dávila', 28, 'Medellín', 'c.davila@gmail.com'),
+('Miguel', 'Guacarapare', 26, 'Cali', 'g.miguel@outlook.com'),
+('Diana', 'Palmeth', 20, 'Soledad', 'diana.palmeth@gmail.com'),
+('María', 'López', 31, 'Barranquilla', 'maria.lopez@hotmail.es');
+
+-- 7. Ingresar datos a la tabla productos
+INSERT INTO productos (nombre_producto, categoria, precio, stock) VALUES
+('Perfume masculino', 'Fragancias', 50.00, 50),
+('Perfume femenino', 'Fragancias', 55.50, 50),
+('Prestobarba', 'Cuidado Corporal', 33.40, 100),
+('Crema hidratante', 'Cuidado Corporal', 28.90, 30),
+('Splash corporal', 'Cuidado Corporal', 25.20, 25),
+('Labial', 'Maquillaje', 30.70 60),
+('Delineador de ojos', 'Maquillaje', 26.30, 20);
+
+-- 8. Insertar datos en la tabla venta
+INSERT INTO ventas (id_cliente, id_producto, fecha_venta, cantidad) VALUES
+(1, 2, '2026-05-01', 8),
+(3, 1, '2026-05-02', 6),
+(5, 5, '2026-05-03', 3),
+(2, 1, '2026-05-04', 4),
+(4, 4, '2026-05-05', 5),
+(5, 6, '2026-05-06', 7),
+(2, 3, '2026-05-07', 1),
+(3, 2, '2026-05-08', 3),
+(1, 6, '2026-05-09', 5),
+(2, 4, '2026-05-10', 2);
+
+-- 9. Consultar datos insertados
+SELECT * FROM clientes;
+
+SELECT * FROM productos;
+
+SELECT * FROM ventas;
+
+
+
+
+
+
+
+
+
